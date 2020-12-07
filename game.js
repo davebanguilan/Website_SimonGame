1b9766ec0e53abe6b8696c88f6cbb0cfd93fd526
davebanguilan <desbanguilan@gmail.com>
2020-12-07 17:44:18 +0800

    Step 1 to 5 done

---
+++ game.js
@@ -0,0 +1,29 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+
+// $("h1").click(inGame);
+$(".btn").click(function () {
+  var userChosenColour = $(this).attr("id");
+  playSound(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+});
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
